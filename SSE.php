5de8fc8cd5ca4602d4c3f4c0d36530b55414ec74
Paulo Leonardo <pauloleonardo.rio@gmail.com>
2022-07-26 11:55:10 -0300

    Primeira versão da classe de SSE

---
+++ SSE.php
@@ -0,0 +1,24 @@
+<?php
+
+class SSE
+{
+    private $data;
+    private $time = 1000;
+
+    private function __construct()
+    {
+        $this->data[] = "";
+    }
+
+    public function setMessage($message)
+    {
+        $this->data[] =  $message;   
+    }
+
+    public function setTime($time)
+    {
+        $this->time = $time;
+    }
+
+
+}

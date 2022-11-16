e847853703b9536825400b30cee11c2a8d961324
Paulo Leonardo <39014913+paulo-leo@users.noreply.github.com>
2022-11-16 17:34:05 -0300

    Update demo.php

--- demo.php
+++ demo.php
@@ -3,44 +3,18 @@
 
  $sse = new SSE;
 
- /*Define o tempo de conexão em segundos*/
- $sse->setTime(10);
 
- /*Define os dados*/
- $sse->setData('nome','Paulo Leonardo');
- $sse->setData('email','plxxxxxxxxxxx@xxxxxxxxxgmail.com');
- $sse->setData('amigos',array('Pedro','Maria','Debora'));
- $sse->setArray(['k1'=>'KKKK','k2'=>'KK22']);
-
- $sse->delete('email');
-
-/*Imprime a resposta do servidor*/
- //$sse->response();
-
- $file = @file_get_contents('https://github.com/paulo-leo/SSE/archive/main.zip');
-
- if($file)
- {
-
-     $z = new ZipArchive();
-
-    // Abrindo o arquivo para leitura/escrita
-     $abriu = $z->open($file);
-    if ($abriu === true) {
+    $sse->setTime(3);
+    $sse->setAction('b');
+    $sse->setData('id','10');
+    $sse->setData('name','Notificação');
+    $sse->setData('email','paulo@gmail.com');
+    $sse->setData('telefone','2198904458');
+    $sse->setData('amigos',array('Pedro','Maria','Debora'));
+    $sse->setArray(['k1'=>'KKKK','k2'=>'KK22']);
     
-        // Obtendo o conteudo de um arquivo pelo nome
-        $conteudo_txt = $z->getFromName('SSE.php');
-    
-        // Obtendo o conteudo de um arquivo pelo indice
-        $conteudo_php = $z->getFromIndex(2);
-    
-        // Salvando o arquivo
-        $z->close();
-    
-    } else {
-        echo 'Erro: '.$abriu;
-    }
- }
+
+    $sse->response(10 > 5);
 
 
 
